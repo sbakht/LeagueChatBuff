fc4699b1671a4d33b381c6a47765903a38ff319a
Saad Bakht <rotk13@gmail.com>
2014-04-22 16:38:48 -0400

    Automatically accepts friend requests

--- src/Main.java
+++ src/Main.java
@@ -13,7 +13,7 @@ public class Main {
 	
 	public static void main(String args[]) {
 
-		LolChat api = new LolChat(ChatServer.NA, false);
+		LolChat api = new LolChat(ChatServer.NA, true);
 		
 		if (api.login("sealsmurf1", "sealsmurf2")) {
 
